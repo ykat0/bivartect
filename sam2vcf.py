5af74af4f366bd2fba360c535a4858fedcace16c
Yuki Kato <ykato@gemini2.local>
2019-06-19 16:36:23 +0900

    Update

--- sam2vcf.py
+++ sam2vcf.py
@@ -1,20 +1,26 @@
 #!/usr/bin/env python
 
-# sam2vcf.py ver. 1.0.6
-# Copyright (C) 2018 Yuki Kato
-# This script is used to convert a mapping result SAM file for Bivartect into a sorted list of predicted variants in VCF format.
+# sam2vcf.py ver. 1.0.7
+# Copyright (C) 2019 Yuki Kato
+# This script is used to convert SAM alignments for Bivartect into predicted VCF variants.
 # The output will be shown in standard out.
-# Note: structural variants on the negative strand will appear on the positive strand in the VCF output. The leftmost position (POS) with "IMPRECISE" INFO is a best approximate due to the Bivartect's prediction on the negative strand.
+# The variation class (VC) consists of:
+# SNV: single nucleotide variant
+# DIV: deletion/insertion variant
+# SV: structural variant of length >= 50 bp
+# BP: unassigned breakpoint
+# Note: small indels and structural variants on the reverse strand will appear on the forward strand in the VCF output. The leftmost position (POS) with "IMPRECISE" INFO is a best approximate due to the Bivartect's prediction on the reverse strand.
 # Usage: Type "./sam2vcf.py -h" in your terminal.
 
 import argparse
 
 parser = argparse.ArgumentParser(
-    usage = './sam2vcf.py [option]* <sam>',
-    description = 'This script is used to convert a mapping result SAM file for Bivartect into a sorted list of predicted variants in VCF format. The output will be shown in standard out. Note: structural variants on the negative strand will appear on the positive strand in the VCF output. The leftmost position (POS) with "IMPRECISE" INFO is a best approximate due to the Bivartect\'s prediction on the negative strand.',
-    epilog = 'sam2vcf.py ver. 1.0.6')
-parser.add_argument('sam', metavar = 'sam <STRING>', type = str,
-                    help = 'path to the mapping result SAM file for Bivartect')
+    formatter_class=argparse.RawDescriptionHelpFormatter,
+    usage='./sam2vcf.py [option]* <sam>',
+    description='sam2vcf.py ver. 1.0.7\n\nThis script is used to convert SAM alignments for Bivartect into predicted VCF variants.\nThe output will be shown in standard out.\n\nThe variation class (VC) consists of:\n\n SNV: single nucleotide variant\n DIV: deletion/insertion variant\n SV: structural variant of length >= 50 bp\n BP: unassigned breakpoint\n\nNote: small indels and structural variants on the reverse strand will appear on the forward strand in the VCF output.\nThe leftmost position (POS) with "IMPRECISE" INFO is a best approximate due to the Bivartect\'s prediction on the reverse strand.'
+)
+parser.add_argument('sam', metavar='sam <STR>', type=str,
+                    help='path to the SAM alignments for Bivartect')
 args = parser.parse_args()
 
 import re
@@ -47,6 +53,8 @@ r1 = re.compile(r"@")
 r2 = re.compile(r"(\S+)\s+(\d+)\s+(\S+)\s+(\d+)\s+(\d+)\s+(\S+)\s+(\S+)\s+(\d+)\s+(\d+)\s+(\w+)")
 r3 = re.compile(r"(\d+):(\w+):([\w\-\*]+):([\w\-\*]+)")
 vcf = {} # Dictonary of {(CHROM, POS, REF, ALT):[ID, QUAL, FILTER, INFO]}
+char_numbers = {"X":97, "Y":98, "MT":99} # Dictionary of {chars:int}
+number_chars = {97:"X", 98:"Y", 99:"MT"} # Dictionary of {int:chars}
 
 with open(args.sam, "r") as f:
     for line in f:
@@ -62,7 +70,7 @@ with open(args.sam, "r") as f:
                 pos = int(m2.group(4))
                 seq = m2.group(10)
                 bp = 0
-                num = qname
+                id = 0
                 ref = "."
                 alt = "."
                 info = "."
@@ -74,86 +82,109 @@ with open(args.sam, "r") as f:
                 # Case: unmapped
                 #if flag == 4 or flag == 77 or flag == 141:
                     #bp = 0
+                
+                chrnum = 0
 
-                if rname == "X":
-                    rname = 97
-                elif rname == "Y":
-                    rname = 98
-                elif rname == "MT":
-                    rname = 99
+                if rname == "X" or rname == "Y" or rname == "MT":
+                    chrnum = char_numbers[rname]
+                
+                else:
+                    chrnum = int(rname)
                 
                 m3 = r3.match(qname)
                 
                 if m3:
                     # Parse a Bivartect output header
-                    num = m3.group(1)
+                    id = int(m3.group(1))
+                    vc = m3.group(2)
                     ref = m3.group(3)
                     alt = m3.group(4)
-                    info = "SVTYPE=" + m3.group(2)
 
                     # Set appropriate bp, ref, alt & info depending on SVTYPE & strand
                     # Case: SNV
-                    if m3.group(2) == "SNV":
-                        if flag == 16: # Negative strand
+                    if vc == "SNV":
+                        info = "VC=SNV" # VC: variation class
+
+                        if flag == 16: # Reverse strand
                             bp = pos - 1
                             ref = revComp(ref)
                             alt = revComp(alt)
-                        else: # Positive strand
+                        else: # Forward strand
                             bp = pos + len(seq)
                     
                     # Case: INS
                     elif ref == "-":
-                        if flag == 16: # Negative strand
+                        if "*" in alt:
+                            info = "VC=SV"
+                        else:
+                            info = "VC=DIV" # DIV: deletion/insertion variant
+                        
+                        if flag == 16: # Reverse strand
                             bp = pos # Best estimate
                             ref = revComp(seq[-1])
                             alt = revComp(seq[-1] + alt)
                             info += ";IMPRECISE"
-                        else: # Positive strand
+                        else: # Forward strand
                             bp = pos + len(seq) - 1
                             ref = seq[-1]
                             alt = seq[-1] + alt
                     
                     # Case: DEL
                     elif alt == "-":
-                        if flag == 16: # Negative strand
+                        if "*" in ref:
+                            info = "VC=SV"
+                        
+                        else:
+                            info = "VC=DIV"
+                        
+                        if flag == 16: # Reverse strand
                             bp = pos - len(ref) # Best estimate
                             ref = revComp(seq[-1] + ref)
                             alt = revComp(seq[-1])
                             info += ";IMPRECISE"
-                        else: # Positive strand
+                        else: # Forward strand
                             bp = pos + len(seq) - 1
                             ref = seq[-1] + ref
                             alt = seq[-1]
 
                     # Case: BP
                     else:
-                        if flag == 16: # Negative strand
+                        info = "VC=BP" # BP: unassigned breakpoint
+
+                        if flag == 16: # Reverse strand
                             bp = pos - len(ref) # Best estimate
                             ref = revComp(seq[-1] + ref)
                             alt = revComp(seq[-1] + alt)
                             info += ";IMPRECISE"
-                        else: # Positive strand
+                        else: # Forward strand
                             bp = pos + len(seq) - 1
                             ref = seq[-1] + ref
                             alt = seq[-1] + alt
                 
-                tup = (int(rname), bp, ref, alt)
+                locus = (chrnum, bp, ref, alt)
                 
-                if not tup in vcf:
-                    vcf[tup] = [num, ".", ".", info]
+                if not locus in vcf:
+                    vcf[locus] = [id, ".", ".", info]
 
 # Output
 print("#CHROM\tPOS\tID\tREF\tALT\tQUAL\tFILTER\tINFO")
 
-for t in sorted(vcf, key=operator.itemgetter(0, 1)):
-    if t[0] == 97:
-        chrom = "X"
-    elif t[0] == 98:
-        chrom = "Y"
-    elif t[0] == 99:
-        chrom = "MT"
-    else:
-        chrom = t[0]
+for locus in sorted(vcf, key=operator.itemgetter(0, 1)):
+    chrchar = ""
+
+    if locus[0] == 97 or locus[0] == 98 or locus[0] == 99:
+        chrchar = number_chars[locus[0]]
     
-    print(chrom, t[1], vcf[t][0], t[2], t[3], vcf[t][1], vcf[t][2], sep = '\t', end = '\t')
-    print(vcf[t][3])
+    else:
+        chrchar = str(locus[0])
+    
+    pos = locus[1]
+    id = vcf[locus][0]
+    ref = locus[2]
+    alt = locus[3]
+    qual = vcf[locus][1]
+    filter = vcf[locus][2]
+    info = vcf[locus][3]
+
+    print(chrchar, pos, id, ref, alt, qual, filter, sep='\t', end='\t')
+    print(info)
